90e57cc1fa2ec47c538c51e9e898afa787780cef
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-06-25 11:50:13 +0200

    feat: add examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CarouselModule } from './carousel';
+import { CarouselWithAutoplayComponent } from './carousel/carousel-with-autoplay.component';
+import { CarouselWithButtonsComponent } from './carousel/carousel-with-buttons.component';
+import { CarouselWithDescriptionComponent } from './carousel/carousel-with-description.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    RouterModule.forRoot([
+      { path: 'with-autoplay', component: CarouselWithAutoplayComponent },
+      { path: 'with-description', component: CarouselWithDescriptionComponent },
+      { path: 'with-buttons', component: CarouselWithButtonsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
